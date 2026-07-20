7865f851d8ae441ec7228700d6fa450c9ffa26d9
rubi1011suraj-coder <rubi1011suraj@gmail.com>
2026-07-20 15:21:22 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,10 @@
 let mantis = document.getElementById("mantis");
+let pipeTop = document.getElementById("pipeTop");
+let pipeBottom = document.getElementById("pipeBottom");
 
+let pipeX = 350;
+let gap = 170;
+let topHeight = 150;
 let y = 250;
 let velocity = 0;
 let gravity = 0.5;
@@ -21,7 +26,22 @@ function update() {
     }
 
     mantis.style.top = y + "px";
+pipeX -= 2;
 
+pipeTop.style.left = pipeX + "px";
+pipeBottom.style.left = pipeX + "px";
+
+if (pipeX < -60) {
+
+    pipeX = 350;
+
+    topHeight = Math.floor(Math.random() * 220) + 50;
+
+    pipeTop.style.height = topHeight + "px";
+
+    pipeBottom.style.height = (600 - topHeight - gap) + "px";
+
+}
     requestAnimationFrame(update);
 }
 
